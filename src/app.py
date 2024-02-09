bd421858e6604ffc2daa18f9a76b8bf5c422d8d3
Samir <98545750+Samir221@users.noreply.github.com>
2024-02-09 17:01:50 +0000

    templates

--- src/app.py
+++ src/app.py
@@ -1,7 +1,10 @@
 from flask import Flask, request, render_template
 from pickle import load
 
-app = Flask(__name__)
+
+# Define the Flask app and set the template folder path
+app = Flask(__name__, template_folder='../templates')
+
 model = load(open("../models/decision_tree_classifier_default_42.sav", "rb"))
 class_dict = {
     "0": "Iris setosa",
@@ -9,19 +12,20 @@ class_dict = {
     "2": "Iris virginica"
 }
 
-@app.route("/", methods = ["GET", "POST"])
+
+@app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
-        
+
         val1 = float(request.form["val1"])
         val2 = float(request.form["val2"])
         val3 = float(request.form["val3"])
         val4 = float(request.form["val4"])
-        
+
         data = [[val1, val2, val3, val4]]
         prediction = str(model.predict(data)[0])
         pred_class = class_dict[prediction]
     else:
         pred_class = None
-    
-    return render_template("index.html", prediction = pred_class)
+
+    return render_template("index.html", prediction=pred_class)
