f13cc8fc52594a140dd476a8381b3c9dbbee71e0
Samir <samir.chawla6@gmail.com>
2024-10-16 10:07:04 -0500

    stuff

--- src/app.py
+++ src/app.py
@@ -5,7 +5,9 @@ from pickle import load
 # Define the Flask app and set the template folder path
 app = Flask(__name__, template_folder='../templates')
 
-model = load(open("../models/decision_tree_classifier_default_42.sav", "rb"))
+# Get the absolute path to the model file
+model_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../models/decision_tree_classifier_default_42.sav")
+model = load(open(model_path, "rb"))
 
 class_dict = {
     "0": "Iris setosa",
